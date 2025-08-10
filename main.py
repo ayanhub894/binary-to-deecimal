6b9bf6a62804d75eb77f5a08b4cd29f22b0861ed
ayanhub894 <ayan.yousuf68@gmail.com>
2025-08-10 17:09:30 +0500

    intial commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 binary = input("Enter a binary number:")  
 length = len(binary) 
 result = 0  
-1
+
 for b in binary:
    int(b) 
    value = 2 ** (length - 1)  
@@ -13,4 +13,4 @@ for b in binary:
    result += final_value  
    length -= 1
 
-print("The decimal value is:", result)
+print("The decimal value is:", result) 
